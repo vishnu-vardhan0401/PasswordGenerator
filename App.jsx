ff84fde061884923c31b8b1e9c8eb56972cdf496
vishnu-vardhan0401 <147438443+vishnu-vardhan0401@users.noreply.github.com>
2024-02-12 19:48:54 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,26 +1,9 @@
 import { useState } from 'react'
-import Navbar from './components/navbar'
-import {BrowserRouter as Router, Routes,Link, Route} from  "react-router-dom";
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import Skills from './pages/Skills'
-import AboutMe from './pages/Aboutme'
 import Passwordgen from './components/passwordgenerater';
 function App() {
-   
   return (
     <div className='App'>
-    {/* <Router>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/skills' element={<Skills/>}/>
-        <Route path='/projects' element={<Projects/>}/>
-        <Route path='/AboutMe' element={<AboutMe/>}/>
-      </Routes>
-    </Router> */}
      <Passwordgen/>
-
     </div>
   )
 }
